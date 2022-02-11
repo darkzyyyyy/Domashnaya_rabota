6455f605e306f4985aae545743a84fd530a55e82
Руслан <@WIN-F52HFKED8TO>
2022-02-12 01:19:56 +0300

    Добавьте файлы проекта.

---
+++ App2/App2/Page/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using App2.Viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.Page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class Page1 : ContentPage
+    {
+        private ViewmodelPage viewmodel = new ViewmodelPage();
+
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = viewmodel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
---
+++ App2/App2/Viewmodel/ViewmodelPage.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App2.Viewmodel
+{
+    public class Root
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+    public class ViewmodelPage:INotifyPropertyChanged
+    {
+        private const string url = "https://jsonplaceholder.typicode.com/todos";
+        private HttpClient _Client = new HttpClient();
+        private ObservableCollection<Root> _Todos;
+        public ObservableCollection<Root> _todos
+        {
+            get => _Todos;
+            set
+            {
+                _Todos = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public async Task Output()
+        {
+            var content = await _Client.GetStringAsync(url);
+            var root = JsonConvert.DeserializeObject<List<Root>>(content);
+            _todos = new ObservableCollection<Root>(root);
+        }
+        
+        public ICommand LoadData => new Command(async value =>
+        {
+            await Output();
+        });
+    }
+}
